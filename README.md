73b1f462003d4e3f7e1ab1a1b6d3c9667e62525a
Oleg Churilo <feauturum@gmail.com>
2021-08-20 14:16:09 +0300

    feat: add some changes

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# Simple Timer
+<img src="icon.png" align="right" />
+<h2 style="text-align: center;">Simple Timer</h2>
 
-[watch](https://oleg-kolosov.github.io/simple-timer/)
+#### CONTENTS  💻
+
+> Create simple timer
+
+#### TOOLS & FEAUTURES 🛠
+
+* Custom propertys
+* Mobile first
+* Animate SVG
+* BEM
+* ES6
+
+
+#### DEMO  🖥
+
+> [watch](https://oleg-kolosov.github.io/simple-timer/)
+
+
+#### AUTHOR  👨‍💻
+
+[Oleg Kolosov](https://github.com/Oleg-Kolosov)
+
+
+#### LICENSE  📜
+
+This project is open source and available under the [MIT License](https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt).
+
--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const startBtn = document.getElementById('btn-start');
 const pauseBtn = document.getElementById('btn-pause');
 const resetBtn = document.getElementById('btn-reset');
 const timerCount = document.getElementById('timer-count');
+const progressCircle = document.querySelector('.timer__progress-ring');
 
 // CONSTANTS
 const INTERVAL_TIME = 1000;
 
 // GLOBAL
-let count = 0;
+let timeCount = 0;
 let intervalId = null;
+
+// PROGRESS CIRCLE
+let radius = progressCircle.r.baseVal.value;
+let circumference = radius * 2 * Math.PI;
+progressCircle.style.strokeDasharray = circumference;
+
+const setProgress = percent => {
+    progressCircle.style.strokeDashoffset =
+        circumference - (percent / 100) * circumference;
+};
 
 // HELPER FUNCTIONS
 const disableBtn = btn => btn.setAttribute('disabled', 'disabled');
@@ -17,28 +28,36 @@ const enableBtn = btn => btn.removeAttribute('disabled', 'disabled');
 
 // ADD EVENT HANDLERS
 startBtn.addEventListener('click', () => {
+    // set interval and start timer
     intervalId = setInterval(() => {
-        count += 1;
-        timerCount.textContent = count;
+        timeCount += INTERVAL_TIME / 1000;
+        timerCount.textContent = timeCount;
+        setProgress(timeCount * 1.666);
     }, INTERVAL_TIME);
 
+    // change attributes buttons
     disableBtn(startBtn);
     enableBtn(pauseBtn);
     enableBtn(resetBtn);
 });
 
 pauseBtn.addEventListener('click', () => {
+    // clear interval
     clearInterval(intervalId);
 
+    // change attributes buttons
     enableBtn(startBtn);
     disableBtn(pauseBtn);
 });
 
 resetBtn.addEventListener('click', () => {
+    // clear interval & update values
     clearInterval(intervalId);
     timerCount.textContent = 0;
-    count = 0;
+    timeCount = 0;
+    setProgress(100);
 
+    // change attributes buttons
     disableBtn(resetBtn);
     disableBtn(pauseBtn);
     enableBtn(startBtn);
